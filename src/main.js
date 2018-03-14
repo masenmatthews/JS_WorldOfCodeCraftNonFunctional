2e6690f503a33ae5a8b794c3da63251b3c5970d6
Katie Fujihara and Masen Matthews <katie@>
2018-03-14 09:34:50 -0700

    initialize

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// import { Player } from './pig.js';
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+// exports.playerModule = Player;
